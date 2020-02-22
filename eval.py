595ac8f3dd80ae4112ff001dccff75b8667f960b
crayzy99 <1600017832@pku.edu.cn>
2020-02-22 17:11:50 +0800

    2.22

--- eval.py
+++ eval.py
@@ -25,9 +25,7 @@ parser = argparse.ArgumentParser()
 parser.add_argument('--data_path', type=str, default='../task1/tok/')
 parser.add_argument('--image_feature_dir', type=str, default='../features_resnet50/')
 parser.add_argument('--result_path', type=str, default='./result.txt')
-parser.add_argument('--log_step', type=int , default=10,
-                    help='step size for prining log info')
-parser.add_argument('--model_num', type=int , default=50,
+parser.add_argument('--model_num', type=int , default=46,
                     help='step size for prining log info')
 
 parser.add_argument('--batch_size', type=int, default=1)
@@ -38,18 +36,20 @@ parser.add_argument('--tgt_vocab_path', type=str, default='./models/tgt_vocab.pk
                     help='path for target vocabulary wrapper')
 parser.add_argument('--embed_size', type=int , default=620 ,
                     help='dimension of word embedding vectors')
+parser.add_argument('--image_size', type=int, default=1024)
 parser.add_argument('--encoder_hidden_size', type=int , default=500 ,
                     help='dimension of encoder lstm hidden states')
 parser.add_argument('--decoder_hidden_size', type=int, default=1000)
 parser.add_argument('--num_layer', type=int, default=2)
 parser.add_argument('--cuda_num', type=int, default=0)
+parser.add_argument('--beam_size', type=int, default=12)
 
 args = parser.parse_args()
 
 torch.cuda.set_device(args.cuda_num)
 result_path = args.result_path
-encoder_path = './models/encoder-' + args.attn + str(args.model_num) + '.pkl'
-decoder_path = './models/decoder-' + args.attn + str(args.model_num) + '.pkl'
+encoder_path = './models/encoder-' + str(args.model_num) + '.pkl'
+decoder_path = './models/decoder-' + str(args.model_num) + '.pkl'
 
 with open(args.src_vocab_path, 'rb') as f:
     src_vocab = pickle.load(f)
@@ -59,8 +59,8 @@ with open(args.tgt_vocab_path, 'rb') as f:
 test_data_loader = get_loader(args.image_feature_dir, args.data_path, src_vocab, tgt_vocab,
                                  batch_size=args.batch_size, type='test', shuffle=False)
 
-encoder = Encoder(args.embed_size, args.encoder_hidden_size, src_vocab, num_layers=args.num_layers)
-decoder = AttnDecoder_1(args.embed_size, args.decoder_hidden_size, tgt_vocab, args.image_feature_size)
+encoder = Encoder(args.embed_size, args.encoder_hidden_size, src_vocab)
+decoder = AttnDecoder_1(args.embed_size, args.encoder_hidden_size, tgt_vocab, args.image_size)
 encoder.load_state_dict(torch.load(encoder_path))
 decoder.load_state_dict(torch.load(decoder_path))
 
@@ -80,7 +80,7 @@ for bi, (sources, targets, src_lengths, tgt_lengths, image_features) in enumerat
     targets = targets.to(device)
     image_features = image_features.to(device)
     encoder_output = encoder(sources)
-    output = decoder.sample(encoder_output, image_features, args.maxlen)  # output有<start>和<end>，且是tensor的list
+    output = decoder.sample(encoder_output, image_features, args.maxlen, args.beam_size)  # output有<start>和<end>，且是tensor的list
     ans = ''
     for i, id in enumerate(output[1:-1]):
         ans += tgt_vocab.idx2word[id.item()].decode() + ' '
